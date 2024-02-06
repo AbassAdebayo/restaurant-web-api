e50dca67b1e96ca934e526dcffc3639ac267c609
AbassAdebayo <greatmoh007@gmail.com>
2024-02-06 18:41:33 +0100

    GetAllRolesWithPermissionsRepository

--- Application/Interfaces/Repositories/Modules.RolePermission.Repository/IRolePermissionsRepository.cs
+++ Application/Interfaces/Repositories/Modules.RolePermission.Repository/IRolePermissionsRepository.cs
@@ -12,7 +12,7 @@ namespace Application.Interfaces.Modules.RolePermission.Repository
         Task<Domain.Domain.Modules.RolePermission.Entities.RolePermission> AddRolePermissionAsync(Domain.Domain.Modules.RolePermission.Entities.RolePermission rolePermission);
         Task<IEnumerable<Domain.Domain.Modules.RolePermission.Entities.RolePermission>> AddRolePermissionsAsync(IEnumerable<Domain.Domain.Modules.RolePermission.Entities.RolePermission> rolePermissions);
         Task<Domain.Domain.Modules.RolePermission.Entities.RolePermission> GetRolePermissionByIdAsync(Guid rolePermissionId);
-        Task<IList<Domain.Domain.Modules.RolePermission.Entities.RolePermission>> GetAllRolePermissionsAsync();
+        Task<IList<Domain.Domain.Modules.RolePermission.Entities.RolePermission>> GetAllRolesWithPermissionsAndSubPermissionsAsync();
         Task RemovePermissionFromRoleAsync(Guid roleId, Guid permissionId);
         Task<List<Permission>> GetPermissionsByRoleAsync(Guid roleId);
         Task<bool> UserHasPermissionAsync(Guid userId, string permissionName);
