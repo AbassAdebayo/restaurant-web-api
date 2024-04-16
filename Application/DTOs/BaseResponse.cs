7e58ed83e2edc7b1fe44682649111e407a389597
AbassAdebayo <greatmoh007@gmail.com>
2024-04-16 10:09:52 +0100

    PaymentSetup: PaymentMethods

--- Application/DTOs/BaseResponse.cs
+++ Application/DTOs/BaseResponse.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Application.DTOs.PaymentDtos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,15 +17,19 @@ namespace Application.DTOs
     public class BaseResponse<T>
     {
         public string Message { get; set; }
-
         public bool Status { get; set; }
         public T Data { get; set; }
         public List<T> DataList { get; set; }
 
-       
-
     }
 
+    public class PaymentResponse
+    {
+        public string authorization_url { get; set; }
+        public string access_code { get; set; }
+        public string reference { get; set; }
+        public PaymentDto paymentDto { get; set; }
+    }
 
     
 }
---
+++ Application/Interfaces/Services/Modules.Payment.Service/IPaymentService.cs
@@ -0,0 +1,18 @@
+﻿using Application.DTOs;
+using Application.DTOs.PaymentDtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces.Services.Modules.Payment.Service
+{
+    public interface IPaymentService
+    {
+        Task<BaseResponse<PaymentResponse>> InitiatePayment(CreatePaymentRequestModel model, Guid orderId);
+        Task<BaseResponse<PaymentResponse>> GetTransactionRecieptAsync(string transactionReference);
+        Task<string> VerifyPayment(UpdatePaymentRequestModel model, Guid orderId);
+
+    }
+}
